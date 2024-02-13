95fda6dea23e3d0babdfcb785472153837a0d9fe
FENTON MWANGI <111893400+FENTON11@users.noreply.github.com>
2024-02-13 12:46:48 +0300

    Update page.tsx

--- ecommerce-4080/app/orders/page.tsx
+++ ecommerce-4080/app/orders/page.tsx
@@ -1,7 +1,7 @@
 const OrderPage = () => {
     return (
         <div className="bg-gray-200 p-4">
-            <h1 className="text-3xl font-bold text-orange-500">Products</h1>
+            <h1 className="text-3xl font-bold text-orange-500">Order</h1>
             <p className="text-gray-600">Welcome to the order page.</p>
         </div>
     );
