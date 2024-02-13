aad2f92ed20436687658dbf48ef3a110a64fd029
FENTON MWANGI <111893400+FENTON11@users.noreply.github.com>
2024-02-13 12:25:39 +0300

    Update page.tsx

---
+++ ecommerce-4080/app/orders/page.tsx
@@ -0,0 +1,10 @@
+const OrderPage = () => {
+    return (
+        <div className="bg-gray-200 p-4">
+            <h1 className="text-3xl font-bold text-orange-500">Products</h1>
+            <p className="text-gray-600">Welcome to the products page.</p>
+        </div>
+    );
+    }
+
+export default OrderPage;
