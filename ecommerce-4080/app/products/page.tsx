707409a41892855dd560e07eac043f32e2c98df9
brianmurerwa <125248371+brianmurerwa@users.noreply.github.com>
2024-02-13 12:25:40 +0300

    Update page.tsx

--- ecommerce-4080/app/products/page.tsx
+++ ecommerce-4080/app/products/page.tsx
@@ -1,4 +1,4 @@
-const ProductsPage = () => {
+const loginPage = () => {
     return (
         <div className="bg-gray-200 p-4">
             <h1 className="text-3xl font-bold text-orange-500">Products</h1>
@@ -7,4 +7,4 @@ const ProductsPage = () => {
     );
     }
 
-export default ProductsPage;
+export default loginPage;
